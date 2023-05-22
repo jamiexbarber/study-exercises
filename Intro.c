0a3c58dcf4eeecc0608900644d8ee4390c4d756c
Jamie Barber <jamie.aba@gmail.com>
2023-05-22 15:25:27 -0500

    Through Lesson 3

--- Intro.c
+++ Intro.c
@@ -2,6 +2,23 @@
 
 int main()
 {
-    puts("Hello, world! Jamie did this!");
+    int rows;
+    float a, b, c;
+    printf("Please enter a number of rows, 18 maximum:");
+    scanf("%d", &rows);
+    if(rows > 18){
+        printf("Too high, please try again");
+    }
+    else{
+    printf("Here is what you've entered: %d", rows);
+    }
+
+    
+ /*   a = 15;
+    b = a /2;
+    c = 4 * b;
+
+    printf("Results of a, b and c are: a = %f, b = %f, c = %f" , a, b, c);
+ */
     return 0;
 }
